96626f132fe34670eca99ad84e8ffb49904cb35f
bean661 <85329404+bean661@users.noreply.github.com>
2022-07-13 00:18:52 +0800

    Update jd_fcwb.js

--- jd_fcwb.js
+++ jd_fcwb.js
@@ -69,8 +69,8 @@ await home()
         
     }
     await List(cookiesArr[0])
-    if(totalNum==111){
-        $.log('账号1邀请人数已达111自动停止')
+    if(totalNum==11){
+        $.log('账号1邀请人数已达11自动停止')
         break
     }
 }
